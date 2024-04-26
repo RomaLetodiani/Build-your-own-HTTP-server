16ec671d17c564f47804464ae49500245fe02c08
RomaLetodiani <roman.letodiani03@gmail.com>
2024-04-26 13:45:55 +0400

    Respond with 404 or 200

--- app/main.js
+++ app/main.js
@@ -6,8 +6,16 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    console.log("🚀 ~ socket.on ~ data:", data);
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    const parsedData = data.toString(); // Convert the data to a string
+    console.log("🚀 ~ socket.on ~ parsedData:", parsedData);
+    const path = parsedData.split(" ")[1]; // Extract the path from the request
+    console.log("🚀 ~ socket.on ~ path:", path);
+
+    if (path === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\nHello, World!");
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
   });
 
   socket.on("close", () => {
